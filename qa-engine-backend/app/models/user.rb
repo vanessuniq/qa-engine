4644624526b9ed0596b0644fa300179d4d32141f
vanessuniq <vanessuniq@gmail.com>
2020-09-06 11:00:38 -0400

    configured database and migrate

--- qa-engine-backend/app/models/user.rb
+++ qa-engine-backend/app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
 
-    validates :username, uniqueness: {case_sensitive: false}
+    validates :username, uniqueness: {case_sensitive: false}, length: {minimum:4}
     validates :password, presence: true, length: { minimum: 5}, allow_nil: true
 
 end
