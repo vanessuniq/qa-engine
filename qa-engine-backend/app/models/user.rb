c3c181c554fc32b31ee03e1ea73bca9c2b715bf1
vanessuniq <vanessuniq@gmail.com>
2020-09-05 22:23:53 -0400

    added user validation

--- qa-engine-backend/app/models/user.rb
+++ qa-engine-backend/app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+    has_secure_password
+
+    validates :username, uniqueness: {case_sensitive: false}
+    validates :password, presence: true, length: { minimum: 5}, allow_nil: true
+
 end
