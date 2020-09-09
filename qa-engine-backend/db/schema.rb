e4716f7a672952cf19240c5942eab7e81429603b
vanessuniq <vanessuniq@gmail.com>
2020-09-09 08:04:27 -0400

    created question model with migration and validation

--- qa-engine-backend/db/schema.rb
+++ qa-engine-backend/db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_06_164551) do
+ActiveRecord::Schema.define(version: 2020_09_09_114521) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "questions", force: :cascade do |t|
+    t.string "author"
+    t.string "title"
+    t.string "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "topic"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
