182dbd71991c016c25b516cdb168a7e32100d301
vanessuniq <vanessuniq@gmail.com>
2020-09-12 21:29:08 -0400

    completed answer update and destroy controller action

--- qa-engine-backend/app/controllers/answers_controller.rb
+++ qa-engine-backend/app/controllers/answers_controller.rb
@@ -1,4 +1,5 @@
 class AnswersController < ApplicationController
+  before_action :find_answer, only: [:update, :destroy]
   def index
     answers = Answer.all
     render json: AnswerSerializer.new(answers).serializable_hash
@@ -14,9 +15,15 @@ class AnswersController < ApplicationController
   end
 
   def update
+    if answer.update(answer_params)
+      render json: AnswerSerializer.new(answer).serializable_hash
+    else
+      render json: {errors: answer.errors.full_messages}, status: :not_acceptable 
+    end
   end
 
   def destroy
+    answer.destroy
   end
 
   private
@@ -24,5 +31,11 @@ class AnswersController < ApplicationController
   def answer_params
     params.require(:answer).permit(:author, :body, :question_id)
   end
+
+  def find_answer
+    answer = Answer.find_by(id: params[:id])
+    render json: {error: 'Answer not found'} unless answer
+  end
+  
 
 end
